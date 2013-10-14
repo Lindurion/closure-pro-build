ecf9b6d5eb150f31602595c78b98ca47961c3994
Eric W. Barndollar <eric@EricWBarndollar.com>
2013-10-13 21:53:23 -0700

    Next checkpoint for js-module-manager.

--- test/js-module-manager-test.js
+++ test/js-module-manager-test.js
@@ -15,6 +15,9 @@
 var jsModuleManager = require('../lib/js-module-manager.js');
 
 var should = require('should');
+var shouldContain = require('./test-util.js').shouldContain;
+var shouldHaveSameElements = require('./test-util.js').shouldHaveSameElements;
+var testUtil = require('./test-util.js');
 
 
 //==============================================================================
@@ -65,12 +68,12 @@ function newExpectedOrderingConstraints() {
   return {
     'uncomp.js': [],
     'not_closure.js': ['uncomp.js'],
-    '3p/uncomp_client.js': ['uncomp.js', 'not_closure.js'],
-    '3p/uncomp_common.js': ['uncomp.js', 'not_closure.js'],
+    '3p/uncomp_client.js': ['uncomp.js'],
+    '3p/uncomp_common.js': ['uncomp.js'],
     '3p/jquery.js': ['uncomp.js', 'not_closure.js', '3p/uncomp_client.js',
                      '3p/uncomp_common.js'],
     '3p/common.js': ['uncomp.js', 'not_closure.js', '3p/uncomp_common.js'],
-    '3p/uncomp_server.js': ['uncomp.js', 'not_closure.js'],
+    '3p/uncomp_server.js': ['uncomp.js'],
     'b.js': ['uncomp.js', 'not_closure.js'],
     'explicit_base.js': ['uncomp.js', 'not_closure.js', 'b.js'],
     '3p/closure/base.js': ['uncomp.js', 'not_closure.js', '3p/uncomp_common.js',
@@ -88,13 +91,42 @@ function newExpectedOrderingConstraints() {
 
 
 //==============================================================================
+// Verification Functions
+//==============================================================================
+
+var expectedOrderingConstraints;
+
+function verifyOrderingConstraints(module) {
+  var filesAppearingAfterCurrent = {};
+  var checkAndRecord = function(currentFile) {
+    expectedOrderingConstraints[currentFile].forEach(function(file) {
+      // If file appears in this module, it must be before currentFile.
+      if (filesAppearingAfterCurrent[file]) {
+        should.fail('Invalid file ordering in module ' + module.name +
+            ', <' + file + '> appears after <' + currentFile + '>');
+      }
+    });
+
+    filesAppearingAfterCurrent[currentFile] = true;
+  };
+
+  for (var i = module.dontCompileInputFiles.length - 1; i >= 0; i--) {
+    checkAndRecord(module.dontCompileInputFiles[i]);
+  }
+  for (var j = module.compiledInputFiles.length - 1; j >= 0; j--) {
+    checkAndRecord(module.compiledInputFiles[j]);
+  }
+}
+
+
+//==============================================================================
 // Test Cases
 //==============================================================================
 
 describe('jsModuleManager', function() {
   describe('#calcInputFiles()', function() {
     // Reset state before each test case.
-    var projectOpts, transitiveClosureDeps, expectedOrderingConstraints;
+    var projectOpts, transitiveClosureDeps;
     beforeEach(function() {
       projectOpts = newProjectOptions();
       transitiveClosureDeps = newTransitiveClosureDeps();
@@ -109,21 +141,29 @@ describe('jsModuleManager', function() {
       // The explicit_base module must be first.
       var explicitBaseModule = computedModules[0];
       explicitBaseModule.name.should.equal('explicit_base');
+      shouldHaveSameElements(explicitBaseModule.dontCompileInputFiles,
+          ['uncomp.js', '3p/uncomp_common.js']);
+      shouldHaveSameElements(explicitBaseModule.compiledInputFiles,
+          ['not_closure.js', '3p/common.js', 'b.js', 'explicit_base.js',
+           '3p/closure/base.js', 'a.js', 'c.js']);
+      verifyOrderingConstraints(explicitBaseModule);
 
       // The client and server modules can come in either order.
-      var clientModule, serverModule;
-      if (computedModules[1].name == 'client') {
-        clientModule = computedModules[1];
-        serverModule = computedModules[2];
-      } else {
-        clientModule = computedModules[2];
-        serverModule = computedModules[1];
-      }
-
+      var clientModule = (computedModules[1].name == 'client') ?
+          computedModules[1] : computedModules[2];
       clientModule.name.should.equal('client');
-      serverModule.name.should.equal('server');
+      shouldHaveSameElements(clientModule.dontCompileInputFiles,
+          ['3p/uncomp_client.js']);
+      shouldHaveSameElements(clientModule.compiledInputFiles, ['3p/jquery.js']);
+      verifyOrderingConstraints(clientModule);
 
-      should.fail('Test TODO: Need to verify files.');
+      var serverModule = (computedModules[1].name == 'server') ?
+          computedModules[1] : computedModules[2];
+      serverModule.name.should.equal('server');
+      shouldHaveSameElements(serverModule.dontCompileInputFiles,
+          ['3p/uncomp_server.js']);
+      shouldHaveSameElements(serverModule.compiledInputFiles, ['d.js']);
+      verifyOrderingConstraints(serverModule);
     });
 
     it('leaves files where they are if they are unique across modules');
@@ -136,33 +176,64 @@ describe('jsModuleManager', function() {
 
     it('fails if module says it is always loaded after a non-existent module',
         function() {
-      projectOpts.jsModules['server'].alwaysLoadedAfterModules.push('DNE');
+      projectOpts.jsModules.server.alwaysLoadedAfterModules.push('DNE');
       (function() {
         jsModuleManager.calcInputFiles(projectOpts, transitiveClosureDeps);
       }).should.throwError(/definition for module <DNE>/);
     });
 
     it('fails if modules contain cycles', function() {
-      projectOpts.jsModules['loopy'] = {
+      projectOpts.jsModules.loopy = {
         alwaysLoadedAfterModules: ['server'],
         closureRootNamespaces: [],
         nonClosureNamespacedInputFiles: [],
         dontCompileInputFiles: []
       };
-      projectOpts.jsModules['explicit_base'].alwaysLoadedAfterModules = ['loopy'];
+      projectOpts.jsModules.explicit_base.alwaysLoadedAfterModules = ['loopy'];
 
       try {
         jsModuleManager.calcInputFiles(projectOpts, transitiveClosureDeps);
         should.fail('Was expecting module cycle exception');
       } catch (e) {
-        e.message.indexOf('Dependency cycle').should.not.equal(-1);
-        e.message.indexOf('explicit_base').should.not.equal(-1);
-        e.message.indexOf('loopy').should.not.equal(-1);
-        e.message.indexOf('server').should.not.equal(-1);
+        shouldContain(e.message, 'Dependency cycle');
+        shouldContain(e.message, 'explicit_base');
+        shouldContain(e.message, 'loopy');
+        shouldContain(e.message, 'server');
       }
     });
 
-    it('fails if any module has more than one root module');
-    it('fails if any input file appears with different compilation types');
+    it('fails if any module has more than one root module', function() {
+      projectOpts.jsModules.another_base = {
+        alwaysLoadedAfterModules: [],
+        closureRootNamespaces: [],
+        nonClosureNamespacedInputFiles: [],
+        dontCompileInputFiles: []
+      };
+      projectOpts.jsModules.server.alwaysLoadedAfterModules = ['another_base'];
+      projectOpts.jsModules.trouble = {
+        alwaysLoadedAfterModules: ['client', 'server'],
+        closureRootNamespaces: [],
+        nonClosureNamespacedInputFiles: [],
+        dontCompileInputFiles: []
+      };
+
+      try {
+        jsModuleManager.calcInputFiles(projectOpts, transitiveClosureDeps);
+        should.fail('Was expecting multiple root module exception');
+      } catch (e) {
+        shouldContain(e.message, 'more than 1 root module');
+        shouldContain(e.message, 'explicit_base');
+        shouldContain(e.message, 'another_base');
+      }
+    });
+
+    it('fails if any input file appears with different compilation types',
+        function() {
+      projectOpts.jsModules.explicit_base.dontCompileInputFiles.push('a.js');
+
+      (function() {
+        jsModuleManager.calcInputFiles(projectOpts, transitiveClosureDeps);
+      }).should.throwError(/a\.js.*multiple compilation modes/);
+    });
   });
 });
